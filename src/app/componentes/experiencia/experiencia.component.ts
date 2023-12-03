4f1f77e45a55013cb10022af1a724f364c27a9cb
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-12-03 19:53:22 -0300

    feat: codigo script de seguimiento de GA

--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -10,37 +10,36 @@ import { ExperienciasService } from 'src/app/servicios/experiencias.service';
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
-  styleUrls: ['./experiencia.component.css']
+  styleUrls: ['./experiencia.component.css'],
 })
-
 export class ExperienciaComponent {
   experienciaList: any;
-  experienciaListDBJson:any;
+  experienciaListDBJson: any;
   private experienciaActualizada = new Subject<void>();
   public formulario!: FormGroup;
 
+  constructor(
+    public generalService: GeneralService,
+    private dataService: DataService,
+    private formBuilder: FormBuilder,
+    private experienciaService: ExperienciasService
+  ) {}
 
-  constructor(public generalService: GeneralService,private dataService:DataService, private formBuilder: FormBuilder,private experienciaService: ExperienciasService){
-    
-
-  }
-
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.formulario = this.formBuilder.group({
       id: ['', [Validators.required]],
       titulo: ['', [Validators.required]],
       fechaIni: ['', [Validators.required]],
       fechaFin: ['', [Validators.required]],
       descripcion: ['', [Validators.required]],
-      img: ['', [Validators.required]]
+      img: ['', [Validators.required]],
     });
 
-    this.generalService.obtenerDbjson().subscribe(
-      (response:any) => {
+    this.generalService.obtenerDbjson().subscribe((response: any) => {
       this.experienciaListDBJson = response[0].experienciaList;
     });
-    
-    this.dataService.misExperiencias$.subscribe(data => {
+
+    this.dataService.misExperiencias$.subscribe((data) => {
       this.experienciaList = data;
     });
 
@@ -65,25 +64,23 @@ export class ExperienciaComponent {
         descripcion: experienciaSeleccionada.descripcion,
       });
     }
-
-
-    
   }
 
-  buscarExperienciaPorId(id: number): Experiencia | null{
-    if(this.experienciaList){
-      return this.experienciaList.find((experiencia: Experiencia) => experiencia.id === id);
-    }else{
+  buscarExperienciaPorId(id: number): Experiencia | null {
+    if (this.experienciaList) {
+      return this.experienciaList.find(
+        (experiencia: Experiencia) => experiencia.id === id
+      );
+    } else {
       return null;
     }
   }
-  
 
   mostrarDatos() {
     const idExperienciaSeleccionada = Number(
       (<HTMLSelectElement>document.getElementById('select-idExp')).value
     );
-    
+
     const experienciaSeleccionada = this.buscarExperienciaPorId(
       idExperienciaSeleccionada
     );
@@ -94,7 +91,7 @@ export class ExperienciaComponent {
         titulo: experienciaSeleccionada.titulo,
         fechaIni: experienciaSeleccionada.fechaIni,
         fechaFin: experienciaSeleccionada.fechaFin,
-        descripcion: experienciaSeleccionada.descripcion
+        descripcion: experienciaSeleccionada.descripcion,
       });
     }
   }
@@ -116,35 +113,35 @@ export class ExperienciaComponent {
     );
   }
 
-  eliminarExperiencia(){
+  eliminarExperiencia() {
     const experienciaId = this.formulario.value.id;
-  console.log("eliminar Exp")
-  this.experienciaService.eliminarExperiencia(experienciaId).subscribe(
-    () => {
-      this.experienciaActualizada.next(); // Emitir el Subject para actualizar los datos
+    console.log('eliminar Exp');
+    this.experienciaService.eliminarExperiencia(experienciaId).subscribe(
+      () => {
+        this.experienciaActualizada.next(); // Emitir el Subject para actualizar los datos
 
-      this.formulario.reset();
-    },
-    (error: HttpErrorResponse) => {
-      alert(error.message);
-    }
-  );
+        this.formulario.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
   }
 
-  agregarExperiencia(){
+  agregarExperiencia() {
     const experiencia = this.formulario.value;
-  console.log(experiencia)
-  this.experienciaService.crearExperiencia(experiencia).subscribe(
-    (response: Experiencia) => {
-      console.log(response);
-      this.experienciaList = experiencia;
-      this.experienciaActualizada.next(); // Emitir el Subject para actualizar los datos
+    console.log(experiencia);
+    this.experienciaService.crearExperiencia(experiencia).subscribe(
+      (response: Experiencia) => {
+        console.log(response);
+        this.experienciaList = experiencia;
+        this.experienciaActualizada.next(); // Emitir el Subject para actualizar los datos
 
-      this.formulario.reset();
-    },
-    (error: HttpErrorResponse) => {
-      alert(error.message);
-    }
-  );
+        this.formulario.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
   }
 }
